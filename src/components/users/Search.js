1ae0bf4b35e86372082eca1c40fe223023e6e22d
haanhn_ <anhnguyenx701@gmail.com>
2020-02-04 02:58:32 +0700

    Convert Search to functional compo. w/ useState Hook

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,56 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-    state = {
-        searchedValue: ''
-    };
+const Search = ({ showClear, searchUsers, clearUsers, setAlert }) => {
+    const [searchedValue, setText] = useState('');
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        setAlert: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
-    };
-
-    render() {
-        return (
-            <div>
-                {/* put onSubmit in form tag not button submit tag */}
-                <form className='form' onSubmit={this.onSubmit.bind(this)}>
-                    <input type='text'
-                        name='searchedValue'
-                        placeholder='Search Users...'
-                        value={this.state.searchedValue}
-                        onChange={this.valueOnChange} />
-                    <input type='submit' value='Search' className='btn btn-dark btn-block' />
-                </form>
-                {this.props.showClear &&
-                    (<button className='btn btn-light btn-block' onClick={this.clearUsers}>
-                        Clear
-                    </button>)
-                }
-            </div>
-        );
-    }
-
-    valueOnChange = (e) => this.setState({ [e.target.name]: e.target.value });
+    const valueOnChange = (e) => setText(e.target.value);
 
     //Note when not using arrow function, use bind 
-    onSubmit(e) {
+    const onSubmit = (e) => {
         e.preventDefault();
-        const searchedValue = this.state.searchedValue;
         if (searchedValue === '') {
-            this.props.setAlert('This field is required', 'light');
+            setAlert('This field is required', 'light');
         } else {
-            this.props.searchUsers(searchedValue);
-            this.setState({ searchedValue: '' });
+            searchUsers(searchedValue);
+            setText('');
         }
     }
 
-    clearUsers = () => {
-        this.props.clearUsers();
-    }
+    return (
+        <div>
+            {/* put onSubmit in form tag not button submit tag */}
+            <form className='form' onSubmit={onSubmit}>
+                <input type='text'
+                    name='searchedValue'
+                    placeholder='Search Users...'
+                    value={searchedValue}
+                    onChange={valueOnChange} />
+                <input type='submit' value='Search' className='btn btn-dark btn-block' />
+            </form>
+            {showClear &&
+                (<button className='btn btn-light btn-block' onClick={clearUsers}>
+                    Clear
+                </button>)
+            }
+        </div>
+    );
 }
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    setAlert: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+};
 
 export default Search;
