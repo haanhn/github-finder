60ecb769cb1de040d9469e8583a3b9bb29d77970
haanhn_ <anhnguyenx701@gmail.com>
2020-02-04 17:55:29 +0700

    Convert App to func component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
@@ -9,14 +9,12 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
-class App extends Component {
-  state = {
-    loading: false,
-    user: {},
-    repos: [],
-    users: [],
-    alert: null
-  }
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
   // async componentDidMount() {
   //   console.log('B4 App_compDidMount()');
@@ -33,85 +31,92 @@ class App extends Component {
   //   console.log('After App_compDidMount()');
   // }
 
-  render() {
-    console.log('App_render()');
-    //destructuring this.state
-    const {users, user, repos, loading} = this.state;
-    return (
-      <Router>
-      <div className="App">
-          <Navbar iconClass='fab fa-github-alt' />
-
-          <div className='container'>
-            <Alert alert={this.state.alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers.bind(this)}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert} />
-                    <Users users={users} loading={loading} />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:loginName'
-                render={(abc) => (
-                  <User {...abc} randomName={abc.match} 
-                    getUser={this.getUser} user={user} 
-                    getUserRepos={this.getUserRepos} repos={repos}
-                    loading={loading}/>
-                  // for desctructuring props <User {...abc} getUser={this.getUser} user={user} loading={loading}/>
-                )}
-              />
-            </Switch>
-          </div>
-        </div>
-      </Router>
-    );
-  }
-
   //Search Users
-  searchUsers = async (value) => {
-    this.setState({ loading: true });
+  const searchUsers = async (value) => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/search/users?q=${value}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     const users = res.data.items;
-    this.setState({ users: users, loading: false });
+    // this.setState({ users: users, loading: false });
+    setUsers(users);
+    setLoading(false);
   }
 
   //Get a single User
-  getUser = async (username) => {
-    this.setState({ loading: true });
+  const getUser = async (username) => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ user: res.data, loading: false });
+    // this.setState({ user: res.data, loading: false });
+    setUser(res.data);
+    setLoading(false);
   }
 
   //Get User's repos
-  getUserRepos = async (username) => {
-    this.setState({ loading: true });
+  const getUserRepos = async (username) => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/users/${username}/repos?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ repos: res.data, loading: false });
+    // this.setState({ repos: res.data, loading: false });
+    setRepos(res.data);
+    setLoading(false);
   }
 
   //Clear Users
-  clearUsers = () => {
-    this.setState({ users: [] });
+  const clearUsers = () => {
+    // this.setState({ users: [] });
+    setUsers([]);
+    setLoading(false);
   }
 
   //Set Alert
-  setAlert = (msg, type) => {
-    this.setState({
-      alert: { msg: msg, type: type }
-    });
+  const showAlert = (msg, type) => {
+    // this.setState({
+    //   alert: { msg: msg, type: type }
+    // });
+    setAlert({ msg: msg, type: type });
+    
     setTimeout(() => {
-      this.setState({ alert: null });
+      setAlert(null);
     }, 3000);
   }
+
+
+  console.log('App_render()');
+  return (
+    <Router>
+      <div className="App">
+        <Navbar iconClass='fab fa-github-alt' />
+
+        <div className='container'>
+          <Alert alert={alert} />
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers.bind(this)}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={showAlert} />
+                  <Users users={users} loading={loading} />
+                </Fragment>
+              )}
+            />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/user/:loginName'
+              render={(abc) => (
+                <User {...abc} randomName={abc.match}
+                  getUser={getUser} user={user}
+                  getUserRepos={getUserRepos} repos={repos}
+                  loading={loading} />
+                // for desctructuring props <User {...abc} getUser={this.getUser} user={user} loading={loading}/>
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
